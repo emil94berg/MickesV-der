6ad057600dd99506d14c7a0d51ef4c0370cb5a36
Oscar Lejon <oscar.lejon@campusnykoping.se>
2025-02-18 16:52:25 +0100

    Added Helper class for menu inputs (with Try Catch)

--- MickesVäder/Display.cs
+++ MickesVäder/Display.cs
@@ -14,60 +14,62 @@ namespace MickesVäder
             string insideOutside = "";
 
             Console.Clear();
-            Console.WriteLine("[1] Innomhus [2] Utomhus");
-            var userNumber = Console.ReadKey(true);
-            if(userNumber.KeyChar == '1')
+            int userNumber = Helpers.GetUserIntInput("[1] Innomhus [2] Utomhus [0] Avsluta programmet", 1, 2, 0);
+            if (userNumber == 1)
             {
                 insideOutside = "Inne";
             }
-            else if(userNumber.KeyChar == '2')
+            else if(userNumber == 2)
             {
                 insideOutside = "Ute";
             }
+            else if(userNumber == 0)
+            {
+                Environment.Exit(0);
+            }
 
             Console.Clear();
-            Console.WriteLine("[1] Sortera meny [2] Dagliga/Månadsvis medel temp/fukt [3] Meteorologisk vinter/höst");
-            var userAnswer = Console.ReadKey(true);
-            switch (userAnswer.KeyChar)
+            int userAnswer = Helpers.GetUserIntInput("[1] Sortera meny [2] Dagliga/Månadsvis medel temp/fukt [3] Meteorologisk vinter/höst", 1, 2, 3);
+            switch (userAnswer)
             {
-                case '1':
-                    Console.WriteLine("[1] Sortera efter temp [2] Sortera efter fuktighet" + (insideOutside == "Ute" ? "[3] Sortera efter mögelrisk" : ""));
-                    var userAnswer2 = Console.ReadKey(true);
+                case 1:
+                    int userAnswer2 = Helpers.GetUserIntInput("[1] Sortera efter temp [2] Sortera efter fuktighet" + (insideOutside == "Ute" ? " [3] Sortera efter mögelrisk" : ""),
+                        insideOutside == "Ute" ? new int[] { 1, 2, 3 } : new int[] { 1, 2 }
+                        );
 
                     Console.Clear();
-                    switch (userAnswer2.KeyChar)
+                    switch (userAnswer2)
                     {
-                        case '1':
+                        case 1:
                             DataSet(Calculate.Avg(weatherDatas, true).Where(x => x.Location == insideOutside).OrderByDescending(x => x.Temp));
                             break;
-                        case '2':
+                        case 2:
                             DataSet(Calculate.Avg(weatherDatas, true).Where(x => x.Location == insideOutside).OrderBy(x => x.Moist));
                             break;
-                        case '3':
+                        case 3:
                             DataSet(Calculate.Avg(weatherDatas, true).Where(x => x.Location == insideOutside).OrderBy(x => x.Mold));
                             break;
                     }
 
                     break;
-                case '2':
+                case 2:
                     Console.WriteLine("Vilket datum vill du kolla närmare på?");
                     string selectedDate = Console.ReadLine();
 
                     GetValues.Daily(Calculate.Avg(weatherDatas, true), insideOutside, selectedDate);
                     break;
-                case '3':
+                case 3:
                     int lowerstTemp = 0;
                     int higestTemp = 0;
 
                     Console.Clear();
-                    Console.WriteLine("[1]Vinter [2]Höst ");
-                    var userNumber1 = Console.ReadKey(true);
-                    if (userNumber1.KeyChar == '1')
+                    int userNumber1 = Helpers.GetUserIntInput("[1]Vinter [2]Höst", 1, 2);
+                    if (userNumber1 == 1)
                     {
                         lowerstTemp = -273;
                         higestTemp = 0;
                     }
-                    else if (userNumber1.KeyChar == '2')
+                    else if (userNumber1 == 2)
                     {
                         lowerstTemp = 0;
                         higestTemp = 10;
---
+++ MickesVäder/Helpers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MickesVäder
+{
+    internal class Helpers
+    {
+        public static int GetUserIntInput(string prompt, params int[] validChoices)
+        {
+            Console.WriteLine(prompt);
+
+            while (true)
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+                try
+                {
+                    int result = int.Parse(keyInfo.KeyChar.ToString());
+                    return validChoices.Contains(result) ? result : throw new Exception();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Ogiltigt val, försök igen...");
+                }
+            }
+        }
+    }
+}
